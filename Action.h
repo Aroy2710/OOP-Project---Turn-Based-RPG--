22da438fa4df91357d9064e17b02b0ad5918b95b
Aroy2710 <aroy03000@gmail.com>
2025-10-02 09:25:16 +0930

    Refactored Action, Added Header Guards

--- Action.h
+++ Action.h
@@ -1,10 +1,20 @@
-class  Action
-{
-    public:
-    virtual void basicAttack(Action* entity, int damage) = 0; //attacks entity with a given damage
-    virtual void defend () = 0; // defend against an attack, takes less damage 
-    virtual void passiveSkill() = 0; // buff that is always active in battle 
-    virtual void takeDamage(int damage) = 0; //takes damage from an attack
-    virtual ~Action() = default; //compiler-generated destructor
+#ifndef __ACTION_H__
+#define __ACTION_H__
+
+// Action interface defines common combat behaviors for all entities
+// in the game, such as attacking, defending, taking damage, and passive skills.
+class Action {
+ public:
+  // Attacks given entity with a specified damage
+  virtual void basicAttack(Action* entity, int damage) = 0;  
+  // Defend against an attack; reduces damage from the next attack.
+  virtual void defend() = 0;  
+  // Applies a passive skill that is always active in battle.
+  virtual void passiveSkill() = 0; 
+  // Applies incoming damage, accounting for defense or buffs.
+  virtual void takeDamage(int damage) = 0; 
+  // Default virtual destructor.
+  virtual ~Action() = default;              
 };
- 
+
+#endif // __ACTION_H__
