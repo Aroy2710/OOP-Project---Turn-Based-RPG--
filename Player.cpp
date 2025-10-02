91fe766ed01a35d61a4fbb95a8f18fe4e36ee65e
Aroy2710 <aroy03000@gmail.com>
2025-10-02 10:28:43 +0930

    Started work on the player class , have not finished implementing and testing

---
+++ Player.cpp
@@ -0,0 +1,66 @@
+#include "Player.h"
+string Player::getName()
+{
+    return name;
+    
+}
+
+string Player::getWeapon()
+{
+    
+}
+
+int Player::getAttackStat()
+{
+    
+}
+
+int Player::getDefenseStat()
+{
+    
+}
+
+int Player::getHealthStat()
+{
+    
+}
+
+void Player::setAttackStat(int attackStat)
+{
+    
+}
+
+void Player::setDefenseStat(int defenseStat)
+{
+    
+}
+
+void Player::setHealthStat(int healthStat)
+{
+    
+}
+
+void Player::useUltimateSkill()
+{
+    
+}
+
+void Player::basicAttack(Action* entity, int damage)
+{
+    
+}
+
+void Player::defend()
+{
+    
+}
+
+void Player::passiveSkill()
+{
+    
+}
+
+void Player::takeDamage(int damage)
+{
+    
+}
---
+++ Player.h
@@ -0,0 +1,36 @@
+#ifndef __PLAYER_H__
+#define __PLAYER_H__
+#include "Action.h"
+#include <iostream>
+using namespace std;
+
+class Player: public Action{
+    public:
+    string getName();
+    string getWeapon();
+    int getAttackStat();
+    int getDefenseStat();
+    int getHealthStat();
+    void setAttackStat(int attackStat);
+    void setDefenseStat(int defenseStat);
+    void setHealthStat(int healthStat);
+    void useUltimateSkill();
+    void basicAttack(Action* entity, int damage) override;
+    void defend() override;
+    void passiveSkill() override;
+    void takeDamage(int damage) override;
+
+    protected:
+    string name;
+    string weapon;
+    int attackStat;
+    int defenseStat;
+    bool isDefending;
+    int healthStat;
+    int ultimateCounter;
+    int useCounter;
+    
+
+};
+
+#endif // __PLAYER_H__
