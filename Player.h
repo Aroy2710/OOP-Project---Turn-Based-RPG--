910027a14e81f518a726a09ab425e96d619ef0e7
Aroy2710 <aroy03000@gmail.com>
2025-10-04 12:23:32 +0930

    Refactored player.h

--- Player.h
+++ Player.h
@@ -1,39 +1,53 @@
 #ifndef __PLAYER_H__
 #define __PLAYER_H__
+
 #include "Action.h"
 #include <iostream>
+#include <string>
+
 using namespace std;
 
-class Player: public Action{
-    public:
-    Player();
-    Player(string name);
-    Player(string name,string weapon,int attackStat,int defenseStat, int healthStat);
-    string getName() const;
-    string getWeapon() const;
-    int getAttackStat() const;
-    int getDefenseStat() const;
-    int getHealthStat() const;
-    void setAttackStat(int attackStat);
-    void setDefenseStat(int defenseStat);
-    void setHealthStat(int healthStat);
-    virtual void useUltimateSkill(Action* entity) = 0;
-    void basicAttack(Action* entity, int damage) override;
-    void defend() override;
-    void takeDamage(int damage) override;
-    virtual ~Player() = default;
+class Player : public Action {
+ public:
+  // Constructors
+  Player();  // Default constructor for gameplay
+  Player(string name);  // Name provided
+  Player(string name, string weapon, int attackStat,
+         int defenseStat, int healthStat);  // Fully parameterized
 
-    protected:
-    string name;
-    string weapon;
-    int attackStat;
-    int defenseStat;
-    bool isDefending;
-    int healthStat;
-    int ultimateCounter;
-    int useCounter;
-    
+  // Getters
+  string getName() const;
+  string getWeapon() const;
+  int getAttackStat() const;
+  int getDefenseStat() const;
+  int getHealthStat() const;
 
+  // Setters
+  void setAttackStat(int attackStat);
+  void setDefenseStat(int defenseStat);
+  void setHealthStat(int healthStat);
+  void setWeapon(const string& weapon);
+
+  // Combat methods
+  void basicAttack(Action* entity, int damage) override;
+  void defend() override;
+  void takeDamage(int damage) override;
+
+  // Pure virtual method for ultimate skill
+  virtual void useUltimateSkill(Action* entity) = 0;
+
+  // Controls console output (true = print messages)
+  bool gameText = true;
+
+ protected:
+  string name;
+  string weapon;
+  int attackStat;
+  int defenseStat;
+  bool isDefending;
+  int healthStat;
+  int ultimateCounter;
+  int useCounter;
 };
 
-#endif // __PLAYER_H__
+#endif  // __PLAYER_H__
