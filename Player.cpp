77100380c559f9bc0375a82384edab2ca10f8603
Aroy2710 <aroy03000@gmail.com>
2025-10-04 10:05:39 +0930

    removed passive skill implementation, defined constructor and getter,setter methods

--- Player.cpp
+++ Player.cpp
@@ -1,4 +1,14 @@
 #include "Player.h"
+
+Player::Player(string name,string weapon,int attackStat,int defenseStat, int healthStat)
+{
+    this->name = name;
+    this->weapon = weapon;
+    this->attackStat = attackStat;
+    this->defenseStat = defenseStat;
+    this->healthStat = healthStat;
+    
+}
 string Player::getName()
 {
     return name;
@@ -7,41 +17,43 @@ string Player::getName()
 
 string Player::getWeapon()
 {
+    return weapon;
     
 }
 
 int Player::getAttackStat()
 {
+    return attackStat;
     
 }
 
 int Player::getDefenseStat()
 {
+    return defenseStat;
     
 }
 
 int Player::getHealthStat()
 {
+    return healthStat;
     
 }
 
 void Player::setAttackStat(int attackStat)
 {
+    this->attackStat =attackStat;
     
 }
 
 void Player::setDefenseStat(int defenseStat)
 {
+    this->defenseStat = defenseStat;
     
 }
 
 void Player::setHealthStat(int healthStat)
 {
-    
-}
-
-void Player::useUltimateSkill()
-{
+    this->healthStat = healthStat;
     
 }
 
@@ -55,12 +67,10 @@ void Player::defend()
     
 }
 
-void Player::passiveSkill()
-{
-    
-}
 
 void Player::takeDamage(int damage)
 {
     
 }
+
+
