f43f5a6bfaa84c0c2e631814d4fc260930242889
EugeneKo1 <ek2132362@gmail.com>
2025-10-12 14:03:48 +1030

    Changed to google style

--- healthPotion.cpp
+++ healthPotion.cpp
@@ -1,22 +1,26 @@
 #include "healthPotion.h"
 
+// Default constructor
+// Sets a default name and heal amount for the health potion.
 HealthPotion::HealthPotion() {
-    itemName = "Health Potion";
-    effectValue = 200;
+  itemName = "Health Potion";
+  effectValue = 200;
 }
 
-HealthPotion::HealthPotion(string name, int healAmount) {
-    this->itemName = name;
-    this->effectValue = healAmount;
+// Constructor
+// Initializes the health potion with a custom name and heal amount.
+HealthPotion::HealthPotion(std::string name, int healAmount) {
+  this->itemName = name;
+  this->effectValue = healAmount;
 }
 
+// Uses the health potion on the player.
+// Increases the player's health by the potion's effect value
+// and prints a message to the console.
 void HealthPotion::use(Player* player) {
-    int current = player->getHealth();
-        player->setHealth(current + effectValue);
+  int current = player->getHealth();
+  player->setHealth(current + effectValue);
 
-        std::cout << player->getName() << " used " << itemName
-                  << " and restored " << effectValue << " health!\n";
+  std::cout << player->getName() << " used " << itemName
+            << " and restored " << effectValue << " health!" << std::endl;
 }
-
-
-
