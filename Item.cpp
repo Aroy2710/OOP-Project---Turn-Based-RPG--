abd708d5a9fda8c9022c75a606a4a673893c8ea3
EugeneKo1 <ek2132362@gmail.com>
2025-10-12 13:18:07 +1030

    Added comments

--- Item.cpp
+++ Item.cpp
@@ -1,10 +1,19 @@
 #include "Item.h"
 
+// Constructor
+// Sets the name of the iteem and the amount
+// that it will change  selected stat by
 Item::Item(string name, int value) {
     itemName = name;
     effectValue = value;
 }
 
+// Returns the name of the item
 string Item::getName() const {
     return itemName;
+}
+
+// Changes the name of the item
+void Item::setName(string name) {
+    this->itemName = name;
 }
