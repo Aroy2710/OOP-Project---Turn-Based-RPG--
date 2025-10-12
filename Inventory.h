050a371213d043a29bdb0ec9829e5ae7214a05f2
EugeneKo1 <ek2132362@gmail.com>
2025-10-12 13:03:42 +1030

    Added comments

--- Inventory.h
+++ Inventory.h
@@ -5,22 +5,25 @@
 #include "Item.h"
 using namespace std;
 
+// Forward declaration of th Player class to avoid errors
 class Player;
 
+// The inventory class aggregates items which the player
+// can use to increase their stats.
 class Inventory {
 
     protected:
-        Player* owner;
-        vector<Item*> items;
+        Player* owner;        // Holds a pointer to player as the target for stat changes
+        vector<Item*> items;  // Aggregates item objects
     
     public:
-        Inventory(Player* owner);
-        ~Inventory();
+        Inventory(Player* owner); // Constructor
+        ~Inventory();             // Destructor
 
-        void addItem(Item* item);
-        void removeItem(Item* item);
-        string listItems();
-        void use(Item* item);
+        void addItem(Item* item);    // Adds an item to the inventory
+        void removeItem(Item* item); // Removes an item from the inventory
+        string listItems();          // Displays all the items currently in the inventory
+        void use(Item* item);        // Uses an item
 
 };
 
