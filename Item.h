8c86c22f6ef045ba3ff6259803033d48a2d38ee9
EugeneKo1 <ek2132362@gmail.com>
2025-10-12 13:14:53 +1030

    Added comments

--- Item.h
+++ Item.h
@@ -5,20 +5,25 @@
 #include <iostream>
 using namespace std;
 
+// Forward declaration of Player class to avoid errors
 class Player;
 
+// The base item class defines the virtual use() function
+// The three inherited items will then override this function
+// since they each change a different stat
 class Item {
 
     protected:
-        string itemName;
-        int effectValue;
+        string itemName; // The item's name
+        int effectValue; // The amount that the items will change the selected stat
     
     public:
-        Item(string name, int value);
-        virtual ~Item() = default;
-        virtual void use(Player* target) = 0;
-        string getName() const;
-        void setName(string name);
+        Item(string name, int value); // Constructor
+        virtual ~Item() = default;    // Destructor
+
+        virtual void use(Player* target) = 0; // Defines the use() function that must be overriden
+        string getName() const;               // Prints the name of the item
+        void setName(string name);            // Changes the name of the item
 };
 
 #endif
