7acefee337263a00fb576b5d1ae7521c4e4fdb57
Aroy2710 <aroy03000@gmail.com>
2025-10-22 11:40:02 +1030

    more reformatting!

--- main.cpp
+++ main.cpp
@@ -3,13 +3,13 @@
 #include "UnitTests.h"
 #include "GameManager.h"
 
-// main file to run the tests for player class
+// main file to run the tests and game 
 int main() {
-  UnitTests UnitTest;
-  UnitTest.runNormalOperationTests();
-  UnitTest.runEdgeCaseTests();
+  // UnitTests UnitTest;
+  // UnitTest.runNormalOperationTests();
+  // UnitTest.runEdgeCaseTests();
 
-  // GameManager game;
+  GameManager game;
 
   return 0;
 }
